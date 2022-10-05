348012a2e216ba1d895724818edeb682b80f1fad
AS.Solyankin <AS.Solyankin@gaz-is.ru>
2022-10-05 13:52:21 +0300

    add task 6

--- src/test/java/pages/AdminPage.java
+++ src/test/java/pages/AdminPage.java
@@ -1,6 +1,10 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class AdminPage extends HelperPages {
     public AdminPage(WebDriver driver) {
@@ -8,6 +12,30 @@ public class AdminPage extends HelperPages {
     }
 
     public boolean checkAuthorizationAdmin(){
-        return getWebElement("//a[contains(@href, '/admin/logout.php')]").isDisplayed();
+        return getWebElement("a[href*='/admin/logout.php']").isDisplayed();
     }
+
+    //метод переклбчения по меню админки
+    public void switchOnMenu(String nameBlock){
+        List <WebElement> menu = getWebElements("#app-");
+        for (WebElement el : menu ) {
+            if (el.findElement(By.cssSelector("[class=name]")).getText().equals(nameBlock)){
+                el.click();
+                break;
+            }
+        }
+    }
+
+    //метод переклбчения по субменю админки
+    public void switchOnSubMenu(String nameSubBlock){
+        List <WebElement> subMenu = getWebElements("ul[id=box-apps-menu] ul li");
+        for (WebElement element : subMenu ) {
+            if (element.findElement(By.cssSelector("[class=name]")).getText().equals(nameSubBlock)) {
+                element.click();
+                break;
+            }
+        }
+    }
+
+
 }
--- src/test/java/pages/HelperPages.java
+++ src/test/java/pages/HelperPages.java
@@ -1,15 +1,19 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class HelperPages {
     final WebDriver driver;
     private int timeOutInSecond = 5;
-    protected String baseUrl = "http://192.168.111.46/";
+    //protected String baseUrl = "http://192.168.111.46/";
+    protected String baseUrl = "http://localhost";
 
     public HelperPages(WebDriver driver) {
         this.driver = driver;
@@ -18,40 +22,62 @@ public class HelperPages {
     //Ожмдание видимости элемента на странице
     public void waitElementVisibility(String locator) {
         WebDriverWait wait = new WebDriverWait(driver, timeOutInSecond);
-        wait.until(ExpectedConditions.refreshed(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator))));
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(locator))));
     }
 
     //метод ожидания кликабельности элемента
     public void waitElementToBeClickable(String locator) {
         WebDriverWait wait = new WebDriverWait(driver, timeOutInSecond);
-        wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(By.xpath(locator))));
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(By.cssSelector(locator))));
     }
 
     //получение WebElement по локатору с ожиданием
     public WebElement getWebElement(String locator) {
         waitElementVisibility(locator);
-        return driver.findElement(By.xpath(locator));
+        return driver.findElement(By.cssSelector(locator));
+    }
+
+    //получение WebElement по локатору с ожиданием
+    public List<WebElement> getWebElements(String locator) {
+        waitElementVisibility(locator);
+        return driver.findElements(By.cssSelector(locator));
     }
 
     //Клик кнопки
     public void clickButton(String nameButton) {
-        String locator = "//button[text()='" + nameButton + "']";
+        String locator = "button[value=" + nameButton + "]";
         waitElementToBeClickable(locator);
         getWebElement(locator).click();
     }
 
     //ввод значения в поле по его аттрибуту name
     public void inputInField(String nameField, String value) {
-        String locator = "//input[@name='" + nameField + "']";
+        String locator = "input[name=" + nameField + "]";
         waitElementToBeClickable(locator);
         getWebElement(locator).sendKeys(value);
     }
 
     //метод клика по чекбоксу
     public void clickCheckbox(String nameCheckbox){
-        String locator = "//label[text()='" + nameCheckbox + "']//input[@type='checkbox']";
-        waitElementToBeClickable(locator);
-        getWebElement(locator).click();
+        String locator = "label input[type=checkbox]";
+        if (getWebElements(locator).size()>1){
+            locator = "label input[type=checkbox][name="+ nameCheckbox +"]";
+            waitElementToBeClickable(locator);
+            getWebElement(locator).click();
+        } else {
+            waitElementToBeClickable(locator);
+            getWebElement(locator).click();
+        }
+    }
+
+    // метод наличия элемента
+    public boolean isElementPresent(String locator){
+        try {
+            getWebElement(locator);
+            return true;
+        } catch (NoSuchElementException ex){
+            return false;
+        }
     }
 
 }
